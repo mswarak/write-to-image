5efe2f130abc58ec067cd52f557451d5d4512f1f
Mansur <Mansur@DESKTOP-JMRG9GN>
2022-05-31 07:08:04 +0300

    Adding example for ltr align test

---
+++ examples/01 - ltr align.php
@@ -0,0 +1,11 @@
+<?php
+require_once '../src/Write_To_Image.php';
+$image = new Write_To_Image('img.jpg');
+
+// add text
+$image->text_ltr("hi 01", 25, 50, 50, array(0,0,0), dirname(__FILE__) . "/arial.ttf");
+$image->text_ltr("hi 02", 25, 250, 250, array(0,0,0), dirname(__FILE__) . "/arial.ttf");
+
+// preview image on browser
+$image->preview();
+?>
