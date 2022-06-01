fc442b9bb820c7ea52437e6aab613b75b6c1e41f
Mansur <Mansur@DESKTOP-JMRG9GN>
2022-06-01 21:51:45 +0300

    Adding example file

---
+++ examples/03 - right align.php
@@ -0,0 +1,13 @@
+<?php
+require_once '../src/Write_To_Image.php';
+$image = new Write_To_Image('img.jpg');
+
+// add text
+$text = "السلام عليكم";
+$image->text_center($text, 25, 150, array(0,0,0), dirname(__FILE__) . "/arial.ttf", 0, false);
+$image->text_center($text, 25, 350, array(0,0,0), dirname(__FILE__) . "/arial.ttf", 0, true);
+
+// preview image on browser
+$image->preview();
+
+?>
