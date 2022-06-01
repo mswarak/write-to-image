a2417f0047cc7a28612083c66002413e1d366401
Mansur <Mansur@DESKTOP-JMRG9GN>
2022-06-01 21:49:38 +0300

    Adding example file

---
+++ examples/02 - center align.php
@@ -0,0 +1,11 @@
+<?php
+require_once '../src/Write_To_Image.php';
+$image = new Write_To_Image('img.jpg');
+
+// add text
+$image->text_center("hi 03", 25, 150, array(0,0,0), dirname(__FILE__) . "/arial.ttf");
+$image->text_center("hi 04", 25, 350, array(0,0,0), dirname(__FILE__) . "/arial.ttf");
+
+// save image to file
+$image->save('output.jpg');
+?>
