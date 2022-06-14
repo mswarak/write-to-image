8fd7d26682baa644fd7900766948187fecde9c1d
Mansur <mansour.worak@gmail.com>
2022-06-14 13:28:33 +0300

    Support adding default font

--- examples/03 - right align.php
+++ examples/03 - right align.php
@@ -4,10 +4,13 @@ use mswarak\Write_To_Image;
 
 $image = new Write_To_Image('img.jpg');
 
+// add default font
+$image->set_default_font(dirname(__FILE__) . "/arial.ttf");
+
 // add text
 $text = "السلام عليكم";
-$image->text_center($text, 25, 150, array(0,0,0), dirname(__FILE__) . "/arial.ttf", 0, false);
-$image->text_center($text, 25, 350, array(0,0,0), dirname(__FILE__) . "/arial.ttf", 0, true);
+$image->text_center($text, 25, 150, array(0,0,0), null, 0, false);
+$image->text_center($text, 25, 350, array(0,0,0), null, 0, true);
 
 // preview image on browser
 $image->preview();
