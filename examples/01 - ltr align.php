fb22184889d15b1d0f5a88c6135bb8a6bda65575
Mansur <mansour.worak@gmail.com>
2022-06-14 13:27:13 +0300

    Support adding default font

--- examples/01 - ltr align.php
+++ examples/01 - ltr align.php
@@ -5,9 +5,12 @@ use mswarak\Write_To_Image;
 
 $image = new Write_To_Image('img.jpg');
 
+// add default font
+$image->set_default_font(dirname(__FILE__) . "/arial.ttf");
+
 // add text
-$image->text_ltr("hi 01", 25, 50, 50, array(0,0,0), dirname(__FILE__) . "/arial.ttf");
-$image->text_ltr("hi 02", 25, 250, 250, array(0,0,0), dirname(__FILE__) . "/arial.ttf");
+$image->text_ltr("hi 01", 25, 50, 50);
+$image->text_ltr("hi 02", 25, 250, 250);
 
 // preview image on browser
 $image->preview();
