0b025c30d640c7d343958b3485cca3b4f0bf4fe3
Mansur <mansour.worak@gmail.com>
2022-06-14 13:27:32 +0300

    Support adding default font

--- examples/02 - center align.php
+++ examples/02 - center align.php
@@ -4,9 +4,12 @@ use mswarak\Write_To_Image;
 
 $image = new Write_To_Image('img.jpg');
 
+// add default font
+$image->set_default_font(dirname(__FILE__) . "/arial.ttf");
+
 // add text
-$image->text_center("hi 03", 25, 150, array(0,0,0), dirname(__FILE__) . "/arial.ttf");
-$image->text_center("hi 04", 25, 350, array(0,0,0), dirname(__FILE__) . "/arial.ttf");
+$image->text_center("hi 03", 25, 150);
+$image->text_center("hi 04", 25, 350);
 
 // save image to file
 $image->save('output.jpg');
