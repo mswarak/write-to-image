58bfc16d9350fd5739dfb9a0264a843b51017d2f
Mansur <mansour.worak@gmail.com>
2022-06-17 23:42:14 +0300

    Fixing bug

--- src/Write_To_Image.php
+++ src/Write_To_Image.php
@@ -1,6 +1,32 @@
 <?php
 namespace mswarak;
 
+/**
+* Write_To_Image is a sample class for write text to images
+*
+* Add a text into jpg images with customized align (ltr, center and rtl)
+*
+* Example usage:
+* // load Write_To_Image
+* use mswarak\Write_To_Image;
+* $image = new Write_To_Image('img.jpg');
+* 
+* // add default font
+* $image->set_default_font(dirname(__FILE__) . "/arial.ttf");
+* 
+* // add text
+* $image->text_ltr("hi 01", 25, 50, 50);
+* $image->text_ltr("hi 02", 25, 250, 250);
+* 
+* // preview image on browser
+* $image->preview();
+*
+* @package  Example
+* @author   Mansur Saleh Warak mansour.worak@gmail.com
+* @version  $Revision: 1.2.3 $
+* @access   public
+*/
+
 class Write_To_Image
 {
     /**
