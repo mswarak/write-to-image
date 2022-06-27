f596cb9d42de3eb95d9a9265bed95047a0de5f5e
Mansur <mansour.worak@gmail.com>
2022-06-27 22:01:40 +0300

    Improve convert_to_RGB function

--- src/Write_To_Image.php
+++ src/Write_To_Image.php
@@ -275,7 +275,36 @@ class Write_To_Image
     */
     function convert_to_RGB($hex_string)
     {
-        return hexdec($hex_string);
+        //return hexdec($hex_string);
+        
+        // Gets a proper hex string
+        $hexStr = preg_replace("/[^0-9A-Fa-f]/", '', $hex_string); 
+        $rgbArray = array();
+        
+        if (strlen($hexStr) == 6)
+        {
+            //If a proper hex code, convert using bitwise operation. No overhead... faster
+            $colorVal = hexdec($hexStr);
+            $rgbArray['red'] = 0xFF & ($colorVal >> 0x10);
+            $rgbArray['green'] = 0xFF & ($colorVal >> 0x8);
+            $rgbArray['blue'] = 0xFF & $colorVal;
+        }
+        elseif (strlen($hexStr) == 3)
+        {
+            //if shorthand notation, need some string manipulations
+            $rgbArray['red'] = hexdec(str_repeat(substr($hexStr, 0, 1), 2));
+            $rgbArray['green'] = hexdec(str_repeat(substr($hexStr, 1, 1), 2));
+            $rgbArray['blue'] = hexdec(str_repeat(substr($hexStr, 2, 1), 2));
+        }
+        else
+        {
+            //Invalid hex color code
+            return false;
+        }
+        
+        // returns the rgb string or the associative
+        return array($rgbArray['red'], $rgbArray['green'], $rgbArray['blue']); 
+        //return $returnAsString ? implode($seperator, $rgbArray) : $rgbArray; // returns the rgb string or the associative
     }
     
     /**
